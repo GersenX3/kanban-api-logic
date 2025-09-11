2709c3a09f8e2779cce0232bd1bfa17de3ba3902
GersenX3 <galdl1438@gmail.com>
2025-09-11 16:13:33 -0600

    [UPDATE] CORS enabled in project

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask_jwt_extended import JWTManager
 from .db import db
 from .routes import kanban_bp
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://kanbanuser:supersecretpassword@postgres-kanban/kanbandb"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = "super-secret-key"
